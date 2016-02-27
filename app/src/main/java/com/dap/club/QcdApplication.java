4eeb306175d915acb74db3412c469315633744f5
王大利 <zengkaizy@163.com>
2016-02-27 14:42:11 +0800

    add jks

--- app/src/main/java/com/dap/club/QcdApplication.java
+++ app/src/main/java/com/dap/club/QcdApplication.java
@@ -3,13 +3,12 @@ package com.dap.club;
 import android.app.Application;
 import android.content.Context;
 import com.avos.avoscloud.AVOSCloud;
-import com.dap.club.util.DapLog;
 import com.facebook.drawee.backends.pipeline.Fresco;
 
 /**
  * Created by dap on 16/1/27.
  */
-public class DapApplication extends Application {
+public class QcdApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
@@ -17,7 +16,7 @@ public class DapApplication extends Application {
         initFresco(this);
         AVOSCloud.initialize(this,"1ootsw3y4smje21veqtkn2w6xtk1al2hdn6xs05vvzp0ed4k","p0ir4kljnq05g4jm7udb7u2lrk45cy1c5hawkufgtkcck00p");
     }
-    private static DapApplication instance;
+    private static QcdApplication instance;
 
 
     /**
@@ -25,7 +24,7 @@ public class DapApplication extends Application {
      *
      * @return
      */
-    public static DapApplication getInstance() {
+    public static QcdApplication getInstance() {
         return instance;
     }
 
